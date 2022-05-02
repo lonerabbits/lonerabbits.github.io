a62c7581e4d281de9ef4cb4272d7b09ac72c9c95
Jay Gatsby <62495042+rabbitalone@users.noreply.github.com>
2022-05-02 20:30:09 +0800

    完成语录

---
+++ assets/script/quotations.js
@@ -0,0 +1,15 @@
+function decide(){
+    var quotesshow = document.getElementById("quotesshowinput").value;  
+    document.querySelector("#quotesshow").textContent = quotesshow;
+
+    var quotesfrom = document.getElementById("quotesfrominput").value;  
+    document.querySelector("#quotesfrom").textContent = quotesfrom;
+}
+
+function convert(){
+    html2canvas(document.querySelector("#pic"),{
+        scale: 2.5
+    }).then(function(canvas) {
+        document.body.appendChild(canvas);
+    });
+}
