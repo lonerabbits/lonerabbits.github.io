ba1069a674686cc9ef5271689af0d575c8faa772
Jay Gatsby <62495042+rabbitalone@users.noreply.github.com>
2022-05-02 21:11:25 +0800

    修复移动端CSS

--- assets/script/quotations.js
+++ assets/script/quotations.js
@@ -10,6 +10,12 @@ function convert(){
     html2canvas(document.querySelector("#pic"),{
         scale: 2.5
     }).then(function(canvas) {
-        document.body.appendChild(canvas);
+//        document.body.appendChild(canvas);
+        var imgURL = canvas.toDataURL();
+        var dlink = document.createElement('a');
+        dlink.href = imgURL;
+        document.body.appendChild(dlink);
+        dlink.click();
+        document.body.removeChild(dlink);
     });
 }
