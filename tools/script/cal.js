06baf8a9064a26c0b7a02a0785b0e8e192097a77
Jay Gatsby <62495042+rabbitalone@users.noreply.github.com>
2022-07-27 13:40:53 +0800

    增加计算项目

--- tools/script/cal.js
+++ tools/script/cal.js
@@ -1,8 +1,19 @@
 function run() {
-    // var mass = document.getElementById('mass').value;
+    var mass = document.getElementById('mass').value;
     var concentration = document.getElementById('concentration').value;
     var volume = document.getElementById('volume').value;
     var molecular_weight = document.getElementById('molecular-weight').value;
-    x = concentration * volume * molecular_weight / 1000000;
-    document.getElementById('output').innerHTML = x;
+    var molarity_result;
+    if (mass == '') {
+        molarity_result = concentration * volume * molecular_weight / 1000000;
+        document.getElementById('mass').value = molarity_result;
+    } else if (concentration == '') {
+        molarity_result = mass * 1000000 / volume / molecular_weight;
+        document.getElementById('concentration').value = molarity_result;
+    } else if (volume == '') {
+        molarity_result = mass * 1000000 / concentration / molecular_weight;
+        document.getElementById('volume').value = molarity_result;
+    } else {
+        alert('请正确输入数据');
+    }
 }
